209a92fa1f95fc0ef59cbd2633ec569377062393
Ashpompy <72096682+Ashpompy@users.noreply.github.com>
2020-09-29 13:46:58 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,24 @@
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
-        # # # # #
-        # . # . #
-        # # # # #
-        # . . . #
-        # # # # #
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
         `)
 })
 input.onGesture(Gesture.TiltLeft, function () {
     Random = randint(0, 10)
     basic.showNumber(Random)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # . # .
+        . . # . .
+        `)
 })
 input.onGesture(Gesture.TiltRight, function () {
     Random = randint(0, 100)
